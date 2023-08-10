0d72f7452755ae6a3c3651c17ed5760326ff68c7
Khaled <khaled.ali.architect@gmail>
2023-08-11 02:28:34 +0300

    Creating Category Views
    See: #5

--- BooksWebApp/Controllers/CategoryController.cs
+++ BooksWebApp/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BooksWebApp.Data;
 using BooksWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksWebApp.Controllers
 {
@@ -17,6 +18,133 @@ namespace BooksWebApp.Controllers
         {
             IEnumerable<Category> categoryList = _db.Categories;
             return View(categoryList);
+        }
+
+        // Get
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            try
+            {
+                if (category.Name == category.DisplayOrder.ToString())
+                {
+                    ModelState.AddModelError("OrderError", "The order can not match the Name.");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var obj = _db.Categories.Single(c => c.Name == category.Name);
+                    if (obj == null)
+                    {
+                        _db.Categories.Add(category);
+                        _db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("CustomerError", "This category already exists.");
+                    }
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "An Expected Error!");
+            }
+
+            return View(category);
+        }
+    
+        //Get
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //Post
+        // POST: Categories/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,DisplayOrder,CreatedDateTime")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(category);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _db.Categories.FindAsync(id);
+            _db.Categories.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _db.Categories.Any(c => c.Id == id);
         }
     }
 }
