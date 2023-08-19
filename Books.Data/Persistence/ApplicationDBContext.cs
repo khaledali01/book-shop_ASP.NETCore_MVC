d7032798d4abb744717b055f85affdf7d523493e
Khaled <khaled.ali.architect@gmail>
2023-08-20 01:32:03 +0300

    Updating Products and Author Functionality

--- Books.Data/Persistence/ApplicationDBContext.cs
+++ Books.Data/Persistence/ApplicationDBContext.cs
@@ -4,15 +4,16 @@ using Microsoft.EntityFrameworkCore;
 namespace Books.Data.Persistence
 {
 #pragma warning disable CS8618
-    public class ApplicationDbContext:DbContext
+    public sealed class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Cover> Cover { get; set; }
-
+        public DbSet<Cover> Covers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
     }
 }
