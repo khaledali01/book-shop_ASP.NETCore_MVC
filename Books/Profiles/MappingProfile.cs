89b5ac7bb2bad079e04a8a4871e26eafbe7a0f1d
Khaled <khaled.ali.architect@gmail>
2023-08-20 01:34:19 +0300

    Using Automapper

---
+++ Books/Profiles/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Books.Domain.Entities;
+using Books.Dtos;
+
+namespace Books.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            CreateMap<Product, ProductDto>();
+            CreateMap<CategoryDto, CategoryDto>();
+            CreateMap<Cover, CoverDto>();
+            CreateMap<AuthorDto, AuthorDto>();
+
+
+            // Dto to Domain
+            CreateMap<ProductDto, Product>()
+                .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<CategoryDto, Product>()
+                 .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<CoverDto, Product>()
+                .ForMember(p => p.Id, opt => opt.Ignore());
+
+            CreateMap<AuthorDto, Product>()
+                 .ForMember(p => p.Id, opt => opt.Ignore());
+        }
+    }
+}
--- Books/Program.cs
+++ Books/Program.cs
@@ -2,6 +2,8 @@ using Books.Data.Persistence;
 using Books.Data.UnitOfWork;
 using Books.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,13 +18,39 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 builder.Services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
- 
+
+// Fixing the error "A possible object cycle was detected"
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddApiVersioning();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Books", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Books API V1");
+        options.RoutePrefix = "swagger";
+    });
 }
 else
 {
@@ -38,8 +66,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
